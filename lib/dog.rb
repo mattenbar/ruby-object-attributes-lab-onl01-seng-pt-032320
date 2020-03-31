c5a481d18a247408854faf04281bc1a2a6894a29
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 17:58:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,22 @@
+#defining a new class - needs captial letter to be class
 class Dog
 
+#getter method -  "gets" a property for us
   def name
     @name
   end
 
+  def breed
+    @breed
+  end
+
+#setter method - "sets" a property for us.
   def name= (new_name)
     @name = new_name
   end
-  
+
+  def breed= (new_breed)
+    @breed = new_breed
+  end
+
 end
