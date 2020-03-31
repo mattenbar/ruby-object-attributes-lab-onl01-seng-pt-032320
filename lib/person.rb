20c10b2d1b6613e5e56c536684dfcfe37b9c0633
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 18:03:56 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+# new class called Person
+class Person
+
+#Getter for name instance variable
+  def name
+    @name
+  end
+# Getter for Job instance variable
+  def job
+    @job
+  end
+
+#setter for name instance variable
+  def name= (new_name)
+    @name = new_name
+  end
+
+#setter for job instance variable
+  def job=(new_job)
+    @job = new_job
+  end
+
+end
